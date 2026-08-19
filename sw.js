6acc5581458a6bc18ae54088d9497732db8da4bf
silvacazehenrique-stack <silvacaze.henrique@gmail.com>
2026-08-19 12:12:33 -0300

    Episodios enriquecidos: 107 cenas e 82 personagens (sw v4)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Service worker: guarda o app para funcionar sem internet.
    Troque a VERSAO quando publicar uma atualização. */
-const VERSAO = "westeros-v3";
+const VERSAO = "westeros-v4";
 const ARQUIVOS = [
   "./",
   "./index.html",
